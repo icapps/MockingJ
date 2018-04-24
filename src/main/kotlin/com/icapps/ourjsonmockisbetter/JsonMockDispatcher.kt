3786e538ed4f2c3e1489c5014e2e99f33bb89065
Maarten Van Giel <maarten.vangiel@icapps.com>
2018-04-24 16:53:00 +0200

    Implement wildcards

--- src/main/kotlin/com/icapps/ourjsonmockisbetter/JsonMockDispatcher.kt
+++ src/main/kotlin/com/icapps/ourjsonmockisbetter/JsonMockDispatcher.kt
@@ -3,31 +3,51 @@ package com.icapps.ourjsonmockisbetter
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
+import java.io.File
 import java.nio.charset.Charset
-import java.nio.file.Files
-import java.nio.file.Paths
+import java.util.logging.Logger
+import java.util.regex.Pattern
 
 class JsonMockDispatcher : Dispatcher() {
 
-    override fun dispatch(request: RecordedRequest): MockResponse {
-        val resourceUri = JsonMockDispatcher::class.java.getResource(getResourceUri(request))?.toURI()
-                ?: return MockResponse().setResponseCode(404)
-        val path = Paths.get(resourceUri)
-
-        if (Files.exists(path)) {
-            val fileContents = Files.readAllBytes(path)
-
-            return MockResponse()
-                    .setResponseCode(200)
-                    .setBody(fileContents.toString(Charset.forName("UTF-8")))
-        }
-
-        return MockResponse().setResponseCode(404)
+    companion object {
+        const val RESPONSES_DIRECTORY = "responses/"
     }
 
-    private fun getResourceUri(request: RecordedRequest): String {
+    private val responseFiles = ResourceUtils.getResourceListing(javaClass, RESPONSES_DIRECTORY)
+            .sortedBy { it.path.count { it == '*' } } // Least amount of wildcards go first
+
+    override fun dispatch(request: RecordedRequest): MockResponse {
+        val fileName = getFilenameForRequest(request)
+        val matchedResponse = findResponseFileForFilename(fileName)
+
+        return if (matchedResponse == null) {
+            MockResponse()
+                    .setResponseCode(404)
+        } else {
+            Logger.getLogger(JsonMockDispatcher::class.java.simpleName).info("Matched response ${matchedResponse.name}")
+
+            MockResponse()
+                    .setResponseCode(200)
+                    .setBody(matchedResponse.readText(Charset.forName("UTF-8")))
+        }
+    }
+
+    private fun getFilenameForRequest(request: RecordedRequest): String {
         val resultFilename = request.path.replaceLast("/", "/${request.method}_")
         return "$resultFilename.json"
+    }
+
+    private fun findResponseFileForFilename(uri: String): File? {
+        return responseFiles.firstOrNull {
+            val fileName = it.path.substringAfter(RESPONSES_DIRECTORY)
+            val pattern = "(.*)${fileName.escapeForRegex()}".toRegex()
+            uri.matches(pattern)
+        }
+    }
+
+    private fun String.escapeForRegex(): String {
+        return this.split("*").joinToString(separator = "(.*)") { Pattern.quote(it) }
     }
 
     private fun String.replaceLast(last: String, replacement: String): String {
--- src/test/kotlin/TestMockServer.kt
+++ src/test/kotlin/TestMockServer.kt
@@ -31,21 +31,8 @@ class TestMockServer {
         val url2 = "${baseUrl}users/1"
         val url3 = "${baseUrl}users/2.json"
         val url4 = "${baseUrl}users/1?query=param"
-
-        fun getResponse(url: String): String {
-            val response = okHttp.newCall(Request.Builder()
-                    .url(url)
-                    .get()
-                    .build()).execute()
-
-            val body = response.body()?.string()
-            if (!response.isSuccessful || body == null) {
-                fail("No body received or not successful for url $url")
-                throw IllegalStateException()
-            } else {
-                return body
-            }
-        }
+        val url5 = "${baseUrl}users/3"
+        val url6 = "${baseUrl}users/3?query=param"
 
         val body = getResponse(url1)
         assert(body.contains("Van Giel"))
@@ -61,6 +48,27 @@ class TestMockServer {
         val body4 = getResponse(url4)
         assert(body4.contains("Van Giel"))
         assert(!body4.contains("Van Looveren"))
+
+        val body5 = getResponse(url5)
+        val body6 = getResponse(url6)
+        assert(body5.contains("Verbeeck"))
+        assert(!body5.contains("Van Giel"))
+        assert(body5 == body6)
+    }
+
+    fun getResponse(url: String): String {
+        val response = okHttp.newCall(Request.Builder()
+                .url(url)
+                .get()
+                .build()).execute()
+
+        val body = response.body()?.string()
+        if (!response.isSuccessful || body == null) {
+            fail("No body received or not successful for url $url")
+            throw IllegalStateException()
+        } else {
+            return body
+        }
     }
 
     @Test
@@ -87,6 +95,27 @@ class TestMockServer {
 
         val postBody = getResponse(url, "POST", RequestBody.create(null, "content"))
         assert(postBody.contains("Van Giel POST"))
+
+        val deleteBody = getResponse(url, "DELETE")
+        assert(deleteBody.contains("Van Giel DELETE"))
+    }
+
+    @Test
+    fun testWildcards() {
+        val url1 = "${baseUrl}users/1"
+        val url2 = "${baseUrl}users/3"
+        val url3 = "${baseUrl}users/3?query=param&lol=lol"
+
+        val body1 = getResponse(url1)
+        assert(body1.contains("Van Giel GET"))
+
+        val body2 = getResponse(url2)
+        assert(body2.contains("Verbeeck GET"))
+        assert(!body2.contains("Van Giel GET"))
+
+        val body3 = getResponse(url3)
+        assert(body3.contains("Verbeeck GET"))
+        assert(!body2.contains("Van Giel GET"))
     }
 
 }
